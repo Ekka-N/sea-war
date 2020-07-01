7d4d4bfc8b9745d91b355466a86de817997b4df7
Ekaterina Niki <ekkin3@gmail.com>
2020-07-01 16:39:01 +0300

    Updates

---
+++ precache-manifest.a897791eb3ef598fce99cafc81c1c0fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e17d88d90dafb1bdc9bef292502eb8",
+    "url": "/sea-war/index.html"
+  },
+  {
+    "revision": "ea38868a75f1dfeebb44",
+    "url": "/sea-war/static/css/main.2a62fb3f.chunk.css"
+  },
+  {
+    "revision": "f6636998faa8e598fde3",
+    "url": "/sea-war/static/js/2.b3ff4bd6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sea-war/static/js/2.b3ff4bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea38868a75f1dfeebb44",
+    "url": "/sea-war/static/js/main.72d59917.chunk.js"
+  },
+  {
+    "revision": "8c74364c3585878f95b8",
+    "url": "/sea-war/static/js/runtime-main.20e173be.js"
+  }
+]);
